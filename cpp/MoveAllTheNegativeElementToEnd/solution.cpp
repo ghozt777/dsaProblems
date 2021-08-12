9d02fe237408769c148f3222c7cebc9169dfa933
ghozt <ghozt777.code@gmail.com>
2021-08-13 01:16:47 +0530

    solution to q10

---
+++ cpp/MoveAllTheNegativeElementToEnd/solution.cpp
@@ -0,0 +1,61 @@
+// link to the problem -> https://practice.geeksforgeeks.org/problems/move-all-negative-elements-to-end1813/1#
+
+// Given an unsorted array arr[] of size N having both negative and positive integers. The task is place all negative element at the end of array without changing the order of positive element and negative element.
+
+// Input : 
+// N = 8
+// arr[] = {1, -1, 3, 2, -7, -5, 11, 6 }
+// Output : 
+// 1  3  2  11  6  -1  -7  -5
+
+// Input : 
+// N=8
+// arr[] = {-5, 7, -3, -4, 9, 10, -1, 11}
+// Output :
+// 7  9  10  11  -5  -3  -4  -1
+
+// <*---------------------- SOLUTION -------------------------------*>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> moveAllNegativeElementToEnd(vector<int>);
+void printArray(vector<int>);
+
+int main(){
+    int n;
+    int getVal;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0;i<n;i++){
+       cin>>getVal;
+       arr.push_back(getVal);
+    }
+    cout<<"original : ";printArray(arr);cout<<endl;
+    cout<<"altered : "; printArray(moveAllNegativeElementToEnd(arr));cout<<endl;
+    return 0;
+}
+
+vector<int> moveAllNegativeElementToEnd(vector<int> arr){
+        vector<int> temp;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]>0){
+                temp.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]<0){
+                temp.push_back(arr[i]);
+            }
+        }
+    return temp;
+}
+
+void printArray(vector<int> arr){
+    cout<<"[ ";
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<", ";
+    }
+    cout<<"]";
+    return ;
+}
